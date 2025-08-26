2d46caffe4cd10dc6fb29945f8166b296544e1cd
swagatam408 <mr.swagatam9@gmail.com>
2025-08-26 04:42:32 +0000

    container config done

---
+++ basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId="swagatam" 
+let acccountAdress="tamluk" 
+var accountPassword="12345"  
+// accountId="swagatam2004"
+console.log(accountId) 
+console.table([accountId, accountPassword, acccountAdress]) 
+acccountAdress="mecheda" 
+accountPassword="swagatam2004" 
+console.table([accountId, accountPassword, acccountAdress, userName]) 
+// do not use var , always use let, const is used for declaring constants that can not be changed 
+let userName;
