831fef410c80a9c6530f9276ae34c2e6ee914d2c
swagatam408 <mr.swagatam9@gmail.com>
2025-08-26 05:43:10 +0000

    container config done

--- basics/02_dataTypes.js
+++ basics/02_dataTypes.js
@@ -1,1 +1,17 @@
+// alert(3 + 3),, will not work as we are using node js
+// console.log(3 +3 )  
+let age=19 
+let name="saas" 
+let value= true 
+console.log(typeof age,name) 
+console.log(typeof name)
 
+//data types-----
+// string="swagatam" 
+//number=2 to power 53
+//bigint=used for large number
+//boolean=True or false
+//null=stand aloone value or khali space
+//undefined=when value is not assigned 
+//symbol=unniqueness 
+//objects
