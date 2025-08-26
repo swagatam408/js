06fd2becb4a77b8d22ee692e7129d13e920378ca
swagatam408 <mr.swagatam9@gmail.com>
2025-08-26 11:39:27 +0000

    container config done

---
+++ basics/04_operations.js
@@ -0,0 +1,42 @@
+// console.log(2 + 2); 
+// console.log(2 - 2); 
+// console.log(2 * 2); 
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2); 
+// console.log(1+"2") 
+// console.log("1"+2) 
+// console.log("1"+2+2) 
+// console.log(1+2+"2") 
+// console.log("hello" + "hi")  
+ 
+
+// let userinput=true 
+// console.log(userinput) 
+// output is true 
+// let userinput= true 
+// console.log(++userinput) 
+// console.log(userinput++) 
+// true means 1 and ++ will increase the value by 1
+
+
+// let x=3 
+// y=++x 
+// console.log(x,y) 
+// output is 4,4  
+ 
+// let a=3 
+// b=a++ 
+// console.log(a,b) 
+// output is 4,3 
+
+
+
+let x=3 
+const y=++x 
+console.log(x,y) 
+
+
+let a=3 
+const b=a++ 
+console.log(a,b)
